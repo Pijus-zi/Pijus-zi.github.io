0e8913c5326c980213dfc1499d7ad6450a54475c
Pijus-zi <pijus.ziaugra@gmail.com>
2021-03-25 08:53:06 +0100

    ye

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,24 +1,44 @@
 // JavaScript Document
-var a=5
-var b=10
-console.log(a+b)
-console.log(b-a)
-console.log(b/a)
-console.log(a*b)
-console.log(b/2)
+var header = document.getElementById("header");
+var date = new Date();
+var hours = date.getHours();
 
-if(a==b){
-    console.log("tala er like")
+if (hours >= 5 && hours < 10) {
+    header.innerText = "God morgen";
+}
+
+if (hours >= 11 && hours < 16) {
+    header.innerText = "God dag"
+}
+
+if (hours >= 17 && hours < 18) {
+    header.innerText = "God ettermiddag"
+}
+
+if (hours >= 19 && hours < 23) {
+    header.innerText = "God kveld"
+}
+
+if (hours >= 00 && hours < 04) {
+    header.innerText = "God natt"
+}
+
+
+
+setInterval(displayclock, 500); 
+function displayclock () {
+    var time = new Date();
+    var hrs = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
+
+    if (hrs > 12) {
+        hrs = hrs - 12;
     }
-else if(a<b){
-    console.log("talet er mindre")
-}
-else if(a>b){
-    console.log("talet er større")
-}
 
-var i = 0;
-while(i < 21) {
-    console.log(i);
-    i++;
+    if (hrs == 0) {
+        hrs = 12;
+    }
+
+    document.getElementById('clock').innerHTML = hrs + ':' + min + ':' +sec;
 }
